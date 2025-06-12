cbfa17a7d37c839d85548b10526a0a956cb48b90
Kim Hyemi <qnlqlaqkq120@gmail.com>
2025-06-12 10:12:15 +0900

    init: react+node 초기세팅

---
+++ server/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+require('dotenv').config();
+
+const app = express();
+
+// Middleware
+app.use(helmet());
+app.use(cors());
+app.use(morgan('combined'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.get('/api/health', (req, res) => {
+  res.json({ message: 'Server is running!' });
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Something went wrong!' });
+});
+
+module.exports = app;
